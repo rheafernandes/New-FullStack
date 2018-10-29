188e839dfc8164e331c456efe26a4bae3877947f
rheafernandes <rheafernandeshallu@gmail.com>
2018-10-29 21:48:09 +0530

    Added a font style to the logo class our-logo

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ export default class Navbar extends Component {
     render() {
         return (
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark container-fluid">
-                <a class="navbar-brand text-info" href="#">Friend-Zone</a>
+                <a class="navbar-brand text-info our-logo" href="#">Friend-Zone</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
