447508f4ab3ddf3097a244e573e8207a5ae01a6a
shreya-shah <shreyashah1920@gmail.com>
2018-10-29 18:04:20 +0530

    added margin to jumbotron buttons

--- src/Pages/User.js
+++ src/Pages/User.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import Navbar from '../Components/Navbar';
 import ProfileCard from '../Components/ProfileCard'
+import { Jumbotron } from '../Components/Jumbotron';
 
 export default class User extends Component {
   render() {
